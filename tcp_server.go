3b9f80eb7f796ce65469b97ef9e97d44191a5e45
krisfollegg <kris.follegg@gmail.com>
2023-04-24 07:19:28 +0000

    endringer

--- tcp_server.go
+++ tcp_server.go
@@ -1,61 +1,63 @@
 package main
 
 import (
-        "io"
-        "log"
-        "net"
-        "sync"
-        "github.com/krisfollegg/is105sem03/mycrypt"
+	"io"
+	"log"
+	"net"
+	"sync"
 
+	"github.com/krisfollegg/is105sem03/mycrypt"
 )
 
 func main() {
 
-        var wg sync.WaitGroup
+	var wg sync.WaitGroup
 
-        server, err := net.Listen("tcp", "172.17.0.2:8000")
-        if err != nil {
-                log.Fatal(err)
-        }
-        log.Printf("bundet til %s", server.Addr().String())
-        wg.Add(1)
-        go func() {
-                defer wg.Done()
-                for {
-                        log.Println("før server.Accept() kallet")
-                        conn, err := server.Accept()
-                        if err != nil {
-                                return
-                        }
-                        go func(c net.Conn) {
-                                defer c.Close()
-                                for {
-                                        buf := make([]byte, 1024)
-                                        n, err := c.Read(buf)
-                                        if err != nil {
-                                                if err != io.EOF {
-                                                        log.Println(err)
-                                                }
-                                                return // fra for løkke
-                                        }
-                                        switch msg := string(buf[:n]); msg {
-                                        case "ping":
-                                                _, err = c.Write([]byte("pong"))
-                                        default:
-                                                dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
-                                                log.Println("Dekrypter melding: ", string(dekryptertMelding))
-                                                _, err = c.Write([]byte(string(dekryptertMelding)))
-                                        }
-                                        if err != nil {
-                                                if err != io.EOF {
-                                                        log.Println(err)
-                                                }
-                                                return // fra for løkke
-                                        }
-                                }
-                        }(conn)
-                }
-        }()
-        wg.Wait()
+	server, err := net.Listen("tcp", "172.17.0.2:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("bundet til %s", server.Addr().String())
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			log.Println("før server.Accept() kallet")
+			conn, err := server.Accept()
+			if err != nil {
+				return
+			}
+			go func(c net.Conn) {
+				defer c.Close()
+				for {
+					buf := make([]byte, 1024)
+					n, err := c.Read(buf)
+					if err != nil {
+						if err != io.EOF {
+							log.Println(err)
+						}
+						return // fra for løkke
+					}
+					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+					log.Println("Dekrypter melding: ", string(dekryptertMelding))
+					switch msg := string(dekryptertMelding); msg {
+					case "ping":
+						kryptertMelding := mycrypt.Krypter([]rune("pong"), mycrypt.ALF_SEM03, 4)
+						log.Println("Kryptert melding: ", string(kryptertMelding))
+						_, err = c.Write([]byte(string(kryptertMelding)))
+					default:
+						_, err = c.Write(buf[:n])
+					}
+					if err != nil {
+						if err != io.EOF {
+							log.Println(err)
+						}
+						return // fra for løkke
+					}
+				}
+			}(conn)
+		}
+	}()
+	wg.Wait()
 }
 
